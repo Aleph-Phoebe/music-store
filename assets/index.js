cb90a779b7c4d19219152682034d69e3fcb19f8d
Aleph Phoebe <phbwangui47@gmail.com>
2024-07-09 09:08:57 +0300

    TRIAL

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+let musicList = [
+    {
+        id: 1,
+        musicImageLink: "assets\images\fally-ipupa.jpeg" ,
+        musicTitle: "roi manitou" ,
+        artistName: "fallly ipupa" ,
+        audiolink: "assets\music\Fally Ipupa - Roi Manitou (Clip Officiel).mp3"
+    } 
+
+]
